fb6cb84e5f9bfc9518c6c104dbe6df8135b5b48a
edwinavalos <edwin.a.avalos@gmail.com>
2023-08-05 11:42:05 -0500

    bug(enable fans): Turn fans back on in controller service
    feat(fan control): If fan is triggered leave it on for 5 minutes
    
    chore(relay.pin): Switch pin to private field
    
    bug(soil sensor): Fix misnamed Stemma soil sensor

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/asssaf/stemma-soil-go/soil"
 	"log"
 	"periph.io/x/conn/v3/i2c/i2creg"
@@ -9,36 +8,35 @@ import (
 )
 
 func main() {
-	if _, err := host.Init(); err != nil { // We need to load drivers to work with i2c stuff
+	if _, err := host.Init(); err != nil {
 		panic(err)
 	}
 
-	devAddr := 0x36                           // This is the register that we found when we ran i2cscan
-	i2cPort, err := i2creg.Open("/dev/i2c-1") // This is the filepath that our device is mounted to
+	devAddrs := []int{0x36, 0x37, 0x39}
+	i2cPort, err := i2creg.Open("/dev/i2c-1")
 	if err != nil {
 		log.Panicf("could not open i2c device: %s", err)
 	}
 
-	opts := soil.DefaultOpts // This instantiates default settings for the soil library, the default is already 0x36, but
-	// could change based on if we want to hook up multiple of these, we would need to jump the sensor,
-	// and update this code
-	if devAddr != 0 {
-		if devAddr < 0x36 || devAddr > 0x39 {
-			panic(fmt.Sprintf("given address not supported by device: %x", devAddr))
-		}
+	var moistureSensors []*soil.Dev
+	for _, devAddr := range devAddrs {
+		opts := soil.DefaultOpts
 		opts.Addr = uint16(devAddr)
+		dev, err := soil.NewI2C(i2cPort, &opts)
+		if err != nil {
+			panic(err)
+		}
+		moistureSensors = append(moistureSensors, dev)
 	}
 
-	dev, err := soil.NewI2C(i2cPort, &opts) // instantiate our soil libraries i2c connection
-	if err != nil {
-		panic(err)
+	for i, dev := range moistureSensors {
+		values := soil.SensorValues{} // Create the sensor value struct we will populate in the next line
+		if err := dev.Sense(&values); err != nil {
+			log.Printf("Sensor: %d unable to sense moisture: %s", i+1, err)
+			continue
+		}
+		floatCap := float32(values.Capacitance) // Turn the Capacitance into a float that we can report with float formatting
+		log.Printf("Sensor: %d temperature: %0.2f°C capacitance : %0.2f\n", i+1, values.Temperature.Celsius(), floatCap)
 	}
-	defer dev.Halt() // Close out the connection we create when this function finishes
 
-	values := soil.SensorValues{} // Create the sensor value struct we will populate in the next line
-	if err := dev.Sense(&values); err != nil {
-		log.Fatalf("unable to sense moisture: %s", err)
-	}
-	floatCap := float32(values.Capacitance) // Turn the Capacitance into a float that we can report with float formatting
-	log.Printf("temperature: %0.2f°C capacitance : %0.2f\n", values.Temperature.Celsius(), floatCap)
 }
